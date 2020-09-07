43a4e7f120c740ce392a43f4adf745565cf4f998
Fedor Melekhin <fedormelexin@gmail.com>
2020-09-07 19:59:37 +0500

    setup.py version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="amazoncaptcha_api",
-    version="0.0.1",
+    version="0.0.2",
     author="Fedor Melekhin",
     author_email="fedormelexin@gmail.com",
     description="Simple anti-captcha compatable API (createTask/getTaskResult) to solve the Amazon's text captcha for free",
